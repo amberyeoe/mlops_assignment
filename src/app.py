fe40c5c6511f127317a1e6844fde3f39bcad8f48
Jae <90853880+javerinetan@users.noreply.github.com>
2024-08-20 05:01:46 +0800

    Update

--- src/app.py
+++ src/app.py
@@ -5,10 +5,10 @@ from flask import Flask, request, url_for, redirect, render_template, jsonify
 
 import os
 import pycaret.classification as pc
+import pycaret.regression as pr
 
 # from pycaret.anomaly import *
 import pandas as pd
-import pickle
 import numpy as np
 
 import hydra
@@ -24,11 +24,10 @@ def load_models(cfg: DictConfig):
 
 load_models()
 print(variables.model.mushroom.path)
+print(variables.model.housing.path)
 
+house_model = pr.load_model(variables.model.housing.path)
 mushroom_model = pc.load_model(variables.model.mushroom.path)
-
-# jav_model = load_model("deployment_28042020")
-# jav_cols = ["age", "sex", "bmi", "children", "smoker", "region"]
 
 # gavin_model = load_model("deployment_28042020")
 # gavin_cols = ["age", "sex", "bmi", "children", "smoker", "region"]
@@ -71,27 +70,25 @@ def predict_mushroom_api():
     return jsonify(prediction)
 
 
-# @app.route("/house-price-prediction")
-# def house_page():
-#     int_features = [x for x in request.form.values()]
-#     final = np.array(int_features)
-#     data_unseen = pd.DataFrame([final], columns=jav_cols)
-#     prediction = predict_model(jav_model, data=data_unseen, round=0)
-#     prediction = int(prediction.Label[0])
+@app.route("/house-price-prediction", methods=["POST", "get"])
+def house_page():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns=variables.columns.housing)
+    prediction = pr.predict_model(house_model, data=data_unseen, round=0)
+    prediction = int(prediction.Label[0])
 
-#     return render_template(
-#         "templates/javerine/house_price_prediction.html",
-#         pred="Expected Bill will be {}".format(prediction),
-#     )
+    return render_template("house_price_prediction.html",pred=prediction)
+    
 
 
-# @app.route("/predict-house-api", methods=["POST"])
-# def predict_house_api():
-#     data = request.get_json(force=True)
-#     data_unseen = pd.DataFrame([data])
-#     prediction = predict_model(jav_model, data=data_unseen)
-#     output = prediction.Label[0]
-#     return jsonify(output)
+@app.route("/predict-house-api", methods=["POST"])
+def predict_house_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = pr.predict_model(house_model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
 
 
 if __name__ == "__main__":
